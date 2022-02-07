f7983bd485def6091c70e534044e6043bfd7f62f
Junhyuck Ko <mrbartrns@naver.com>
2022-02-07 18:04:14 +0900

    feat: WIP
    feat: WIP
    
    feat: WIP
    
    feat: Create family designed button

--- frontend/src/components/Button/Button.stories.tsx
+++ frontend/src/components/Button/Button.stories.tsx
@@ -3,39 +3,43 @@ import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { Button } from "./Button";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Example/Button",
+  title: "shared/Button",
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: "color" },
+    variant: {
+      options: ["primary", "text", "default"],
+      control: { type: "radio" },
+    },
+    shape: {
+      options: ["round", "default"],
+      control: { type: "radio" },
+    },
+    size: {
+      options: ["dense", "small", "medium", "large"],
+      control: { type: "radio" },
+    },
   },
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
+export const Default = Template.bind({});
+Default.args = {
+  disabled: false,
+  variant: "default",
+  size: "medium",
+  shape: "default",
+  fullWidth: false,
+  children: "Button Test",
+};
+
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: "Button",
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
-};
-
-export const Large = Template.bind({});
-Large.args = {
+  disabled: false,
+  variant: "primary",
   size: "large",
-  label: "Button",
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: "small",
-  label: "Button",
+  shape: "default",
+  fullWidth: false,
+  children: "Button Test",
 };
--- frontend/webpack/webpack.config.js
+++ frontend/webpack/webpack.config.js
@@ -1,9 +1,9 @@
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common.js");
 
-module.exports = (envVars) => {
-  const { env } = envVars;
-  const envConfig = require(`./webpack.${env}.js`);
+module.exports = () => {
+  const mode = process.env.NODE_ENV;
+  const envConfig = require(`./webpack.${mode}.js`);
   const config = merge(commonConfig, envConfig);
   return config;
 };
