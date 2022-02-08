77e531054e39eee673f9bef8052a4c53bb5258ae
Junhyuck Ko <mrbartrns@naver.com>
2022-02-08 17:10:51 +0900

    feat: Create private route

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,27 +1,5 @@
 import { PropsWithChildren } from "react";
 import * as Styled from "./Button.styles";
-// interface ButtonProps {
-//   /**
-//    * Is this the principal call to action on the page?
-//    */
-//   primary?: boolean;
-//   /**
-//    * What background color to use
-//    */
-//   backgroundColor?: string;
-//   /**
-//    * How large should the button be?
-//    */
-//   size?: 'small' | 'medium' | 'large';
-//   /**
-//    * Button contents
-//    */
-//   label: string;
-//   /**
-//    * Optional click handler
-//    */
-//   onClick?: () => void;
-// }
 interface ButtonProps {
   variant?: "primary" | "text" | "default";
   shape?: "round" | "default";
