445f10e1f26f4261a75bb1263c28c7e0cbe5760a
Junhyuck Ko <mrbartrns@naver.com>
2022-03-05 09:09:22 +0900

    feature: Set use schedule

--- frontend/src/components/pages/CreatePage/CreatePage.tsx
+++ frontend/src/components/pages/CreatePage/CreatePage.tsx
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
+import { useMutation } from "react-query";
+import { useSchedule } from "../../../hooks/useSchedule";
+import api from "../../../utils/instance";
 import { Schedule, SubSchedule } from "../../../types/schedule";
-import { useCreateSchedule } from "../../../hooks/useCreateSchedule";
+import { v4 } from "uuid";
 
 function CreatePage(): JSX.Element {
   const {
     schedule,
-    onCreateSubSchedule,
-    onChangeTitle,
+    setSchedule,
     onChangeSubTitle,
-    onSubmit,
-  } = useCreateSchedule();
+    onChangeTitle,
+    onCreateSubSchedule,
+    onDeleteSubSchedule,
+  } = useSchedule();
+  const mutation = useMutation((data: Schedule<SubSchedule>) =>
+    api.post("/api/data", data),
+  );
+  const onSubmit = useCallback(async (data: Schedule<SubSchedule>) => {
+    try {
+      mutation.mutateAsync(data);
+      setSchedule({
+        title: "",
+        uuid: v4(),
+        checked: false,
+        sub_schedules: [],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
   return (
     <form
       onSubmit={(e) => {
@@ -24,16 +44,20 @@ function CreatePage(): JSX.Element {
       <button onClick={onCreateSubSchedule}>추가하기</button>
       {schedule.sub_schedules.map((subSchedule, idx) => {
         return (
-          <input
-            style={{ display: "inline-block" }}
-            required
-            type="text"
-            key={subSchedule.uuid}
-            onChange={(e) => {
-              onChangeSubTitle(subSchedule.uuid, e);
-            }}
-            value={subSchedule.subtitle}
-          />
+          <div key={subSchedule.uuid}>
+            <input
+              style={{ display: "inline-block" }}
+              required
+              type="text"
+              onChange={(e) => {
+                onChangeSubTitle(subSchedule.uuid, e);
+              }}
+              value={subSchedule.subtitle}
+            />
+            <button onClick={() => onDeleteSubSchedule(subSchedule.uuid)}>
+              삭제
+            </button>
+          </div>
         );
       })}
       <button onClick={() => console.log(schedule)}>제출하기</button>
