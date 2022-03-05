1224d70442df7ae132c171dd43efc6d6833e32fb
Junhyuck Ko <mrbartrns@naver.com>
2022-03-05 09:07:33 +0900

    feature: Separate useSchedule

--- frontend/src/hooks/useCreateSchedule.ts
+++ frontend/src/hooks/useCreateSchedule.ts
@@ -1,49 +1,12 @@
-import { useState, useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import { Schedule, SubSchedule } from "../types/schedule";
 import { useMutation } from "react-query";
 import api from "../utils/instance";
 import { v4 } from "uuid";
+import { useSchedule } from "./useSchedule";
 
 function useCreateSchedule() {
-  const [schedule, setSchedule] = useState<Schedule<SubSchedule>>({
-    title: "",
-    uuid: v4(),
-    checked: false,
-    sub_schedules: [],
-  });
-
-  const onCreateSubSchedule = useCallback(() => {
-    setSchedule((prev) => {
-      return {
-        ...prev,
-        sub_schedules: [
-          ...prev.sub_schedules,
-          { uuid: v4(), subtitle: "", checked: false },
-        ],
-      };
-    });
-  }, []);
-
-  const onChangeTitle = useCallback((e: any) => {
-    setSchedule((prev) => {
-      return {
-        ...prev,
-        title: e.target.value,
-      };
-    });
-  }, []);
-
-  const onChangeSubTitle = useCallback((uuid: string, e: any) => {
-    setSchedule((prev) => {
-      return {
-        ...prev,
-        sub_schedules: prev.sub_schedules.map((sub: SubSchedule) =>
-          sub.uuid === uuid ? { ...sub, subtitle: e.target.value } : sub,
-        ),
-      };
-    });
-  }, []);
-
+  const { schedule, setSchedule } = useSchedule();
   const mutation = useMutation((data: Schedule<SubSchedule>) =>
     api.post("/api/data", data),
   );
@@ -62,10 +25,6 @@ function useCreateSchedule() {
     }
   }, []);
   return {
-    schedule,
-    onCreateSubSchedule,
-    onChangeTitle,
-    onChangeSubTitle,
     onSubmit,
   };
 }
---
+++ frontend/src/hooks/useSchedule.ts
@@ -0,0 +1,73 @@
+import React, { useState, useCallback } from "react";
+import { Schedule, SubSchedule } from "../types/schedule";
+import { useMutation } from "react-query";
+import api from "../utils/instance";
+import { v4 } from "uuid";
+
+function useSchedule() {
+  const [schedule, setSchedule] = useState<Schedule<SubSchedule>>({
+    title: "",
+    uuid: v4(),
+    checked: false,
+    sub_schedules: [],
+  });
+
+  const onCreateSubSchedule = useCallback(() => {
+    setSchedule((prev) => {
+      return {
+        ...prev,
+        sub_schedules: [
+          ...prev.sub_schedules,
+          { uuid: v4(), subtitle: "", checked: false },
+        ],
+      };
+    });
+  }, []);
+
+  const onChangeTitle = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSchedule((prev) => {
+        return {
+          ...prev,
+          title: e.target.value,
+        };
+      });
+    },
+    [],
+  );
+
+  const onChangeSubTitle = useCallback(
+    (uuid: string, e: React.ChangeEvent<HTMLInputElement>) => {
+      setSchedule((prev) => {
+        return {
+          ...prev,
+          sub_schedules: prev.sub_schedules.map((sub: SubSchedule) =>
+            sub.uuid === uuid ? { ...sub, subtitle: e.target.value } : sub,
+          ),
+        };
+      });
+    },
+    [],
+  );
+
+  const onDeleteSubSchedule = useCallback((uuid: string) => {
+    setSchedule((prev) => {
+      return {
+        ...prev,
+        sub_schedules: prev.sub_schedules.filter(
+          (sub: SubSchedule) => sub.uuid !== uuid,
+        ),
+      };
+    });
+  }, []);
+  return {
+    schedule,
+    setSchedule,
+    onCreateSubSchedule,
+    onChangeTitle,
+    onChangeSubTitle,
+    onDeleteSubSchedule,
+  };
+}
+
+export { useSchedule };
